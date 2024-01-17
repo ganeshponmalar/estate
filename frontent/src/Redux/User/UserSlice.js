040b009c371479265b31dca138c2e31188cc5219
ganeshponmalar <ganeshponmalar.j@gmail.com>
2024-01-17 16:49:00 +0530

    First commit

---
+++ frontent/src/Redux/User/UserSlice.js
@@ -0,0 +1,30 @@
+// UserSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  loading: false,
+  error: null,
+  user: null, // You may have other properties specific to the user here
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export default userSlice.reducer;
---
+++ frontent/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home(){
+
+    return(
+
+        <div>
+      Iam from home page
+        </div>
+    )
+}
+
